af2a936db7e6d2f3c6fca516681acb9d62f535bf
mcmicmax <maxime.lorin56@gmail.com>
2022-03-23 16:51:08 +0100

    Add slider

--- js/main.js
+++ js/main.js
@@ -1,26 +1,87 @@
-let num=120;
+let actualIndex=0;
 
-function fuzz(param){
-    let str="";
-    if(param%3===0 ){
-        str+="fizz";
+let imageArray=document.querySelectorAll('.highlight__img');
+let container=document.querySelector(".sliderContainer__dots");
+
+let htmlDot="";
+let sliderLength=imageArray.length;
+
+let btnNext=document.querySelector(".sliderContainer__chevronRight");
+let btnPrev=document.querySelector(".sliderContainer__chevronLeft");
+
+imageArray[actualIndex].classList.add("isOn");
+
+imageArray.forEach((element,i) => {
+    console.log(element, sliderLength,i);
+
+    htmlDot+=`<div class="sliderContainer__dots--dot"></div>`
+});
+container.innerHTML=htmlDot;
+let dot=document.querySelectorAll(".sliderContainer__dots--dot");
+dot[actualIndex].classList.add("isSlct");
+
+btnNext.addEventListener("click", function() {
+    if(actualIndex<sliderLength-1){
+        actualIndex++;
+        dot[actualIndex].classList.add("isSlct");
+        dot[actualIndex-1].classList.remove("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
+        imageArray[actualIndex-1].classList.remove("isOn");
+     
+    }else{
+        dot[actualIndex].classList.remove("isSlct");
+        imageArray[actualIndex].classList.remove("isOn");
+        actualIndex=0;
+        dot[actualIndex].classList.add("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
     }
-    if(i%5===0){
-        str+="buzz";
+});
+
+btnPrev.addEventListener("click", ()=> {
+    if(actualIndex>0){
+        actualIndex--;
+        dot[actualIndex].classList.add("isSlct");
+        dot[actualIndex+1].classList.remove("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
+        imageArray[actualIndex+1].classList.remove("isOn");
+    }else{
+        dot[actualIndex].classList.remove("isSlct");
+        imageArray[actualIndex].classList.remove("isOn");
+        actualIndex=sliderLength-1;
+        dot[actualIndex].classList.add("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
     }
-    if(i%3!=0 &&!i%5!=0){
-        str=param;
+});
+
+dot.forEach((element,i) => {
+    element.addEventListener("click",()=>{
+        imageArray[actualIndex].classList.remove("isOn");
+        dot[actualIndex].classList.remove("isSlct");
+        actualIndex=i
+        imageArray[actualIndex].classList.add("isOn");
+        dot[actualIndex].classList.add("isSlct");  
+    })
+});
+
+let intervalChange=()=>{
+    if(actualIndex<sliderLength-1){
+        actualIndex++;
+        dot[actualIndex].classList.add("isSlct");
+        dot[actualIndex-1].classList.remove("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
+        imageArray[actualIndex-1].classList.remove("isOn");
+     
+    }else{
+        dot[actualIndex].classList.remove("isSlct");
+        imageArray[actualIndex].classList.remove("isOn");
+        actualIndex=0;
+        dot[actualIndex].classList.add("isSlct");
+        imageArray[actualIndex].classList.add("isOn");
     }
-    return str;
 }
+setInterval(intervalChange,2500);
 
-for(i=0;i<num;i++){
-    console.log(fuzz(i))
-}
 
-let test=document.querySelector("h1");
-
-console.log(test.innerText)
 
 
 
